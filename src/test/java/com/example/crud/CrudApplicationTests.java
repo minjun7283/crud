81e8cf5c0ea4d4115ec55e5de30250e8dcb587fe
minjun7283 <minjun7283@naver.com>
2022-12-05 19:32:14 +0900

    crud

--- src/test/java/com/example/crud/CrudApplicationTests.java
+++ src/test/java/com/example/crud/CrudApplicationTests.java
@@ -1,13 +1,18 @@
 package com.example.crud;
 
+import com.example.crud.domain.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
 
 @SpringBootTest
 class CrudApplicationTests {
 
     @Test
     void contextLoads() {
+        ArrayList<User> user= new ArrayList<>();
+        user.add(new User("min","jun","1234"));
     }
 
 }
